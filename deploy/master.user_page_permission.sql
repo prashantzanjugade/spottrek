cd72ff1d663708c925bbc74b7913b175394fcd95
prashantzanjugade <zanjugade.prashant@gmail.com>
2022-11-29 17:34:54 +0530

    Create Table master.user_page_permission

---
+++ deploy/master.user_page_permission.sql
@@ -0,0 +1,20 @@
+-- Deploy spottrek:master.user_page_permission to pg
+-- requires: master.users
+-- requires: master.page
+-- requires: master.client
+
+BEGIN;
+
+create table master.user_page_permission (
+	  id bigserial,
+	  users_id integer not null,
+	  page_id integer not null,
+	  created_on timestamp default now(),	
+	  created_by integer not null,
+	  primary key (id),
+	  foreign key (users_id) references master.users (id),
+	  foreign key (page_id) references master.page (id),
+	  foreign key (created_by) references master.client(id)
+);
+
+COMMIT;
