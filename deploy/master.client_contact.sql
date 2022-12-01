d9ccb54cfe72eefdc69a7ee61b69d070163abdb4
prashantzanjugade <zanjugade.prashant@gmail.com>
2022-12-01 11:57:42 +0530

    Add comment on master.client_contact.client_contact_type column

--- deploy/master.client_contact.sql
+++ deploy/master.client_contact.sql
@@ -3,7 +3,7 @@
 
 BEGIN;
 
-create table master.client_contact (
+/*create table master.client_contact (
 	  id bigserial,
 	  client_id integer not null,
 	  client_contact_type integer not null, -- mail authority, manager, spoc, etc
@@ -15,7 +15,8 @@ create table master.client_contact (
 	  primary key (id),
 	  foreign key (client_id) references master.client(id)
 );
+*/
 
-comment on column master.client.client_type is '1 for Mail Authority, 2 for Manager, 3 for SPOC, 4 for Other';
+comment on column master.client_contact.client_contact_type is '1 for Mail Authority, 2 for Manager, 3 for SPOC, 4 for Other';
 
 COMMIT;
--- revert/master.client_contact.sql
+++ revert/master.client_contact.sql
@@ -1,7 +1,22 @@
--- Revert spottrek:master.client_contact from pg
+-- Deploy spottrek:master.client_contact to pg
+-- requires: master.client
 
 BEGIN;
 
-DROP TABLE IF EXISTS master.client_contact;
+/*create table master.client_contact (
+	  id bigserial,
+	  client_id integer not null,
+	  client_contact_type integer not null, -- mail authority, manager, spoc, etc
+	  name varchar(100) not null,
+	  designation varchar(100),
+	  email varchar(50) not null,
+	  mobile varchar(15),
+	  created_on timestamp default now(),
+	  primary key (id),
+	  foreign key (client_id) references master.client(id)
+);
+*/
+
+comment on column master.client_contact.client_contact_type is null;
 
 COMMIT;
