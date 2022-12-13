bd4cbc092a63ec24cf67e795d7aacffa92983981
prashantzanjugade <zanjugade.prashant@gmail.com>
2022-12-13 15:45:09 +0530

    Default Value set for master.users created_on, status

--- deploy/master.users.sql
+++ deploy/master.users.sql
@@ -9,6 +9,9 @@ alter table master.users drop column department;
 alter table master.users drop column client_id;
 alter table master.users drop column is_all_advertiser;
 
+alter table master.users alter column status set default 1;
+alter table master.users alter column created_on set default now();
+
 comment on column master.users.status is '1 for active, 0 for inactive';
 
 --alter table master.users drop constraint users_client_id;
